c869f60becf3ae68bbd5257c9c1b3ddce583aafa
carlo0o0o <car.nota@stud.uniroma3.it>
2023-07-13 11:50:01 +0200

    Camera

--- My project (4)/Assets/CameraManager.cs
+++ My project (4)/Assets/CameraManager.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] private GameObject myCamera;
+    [SerializeField] private PolygonCollider2D cd;
+    [SerializeField] private Color gizmosColor;
 
+
+    private void Start()
+    {
+        myCamera.GetComponent<CinemachineVirtualCamera>().Follow = GameObject.Find("Player").transform;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.GetComponent<Player>() != null)
@@ -14,6 +22,12 @@ public class CameraManager : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.GetComponent<Player>() != null)
-            myCamera.SetActive(false);       
+            myCamera.SetActive(false);      
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmosColor;
+        Gizmos.DrawWireCube(cd.bounds.center, cd.bounds.size);
     }
 }
