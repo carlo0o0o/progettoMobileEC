9efb4982d44102bbd65b194a2729edd91c3f9a7c
emiliofalco <emi.falco@stud.uniroma3.it>
2023-07-14 18:33:22 +0200

    Frutta pt2

--- My project (4)/Assets/Fruit_Item.cs
+++ My project (4)/Assets/Fruit_Item.cs
@@ -2,8 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum FruitType
+{
+    apple,
+    banana,
+    cherry,
+    kiwi,
+    melon,
+    orange,
+    pineapple,
+    strawberry
+}
+
 public class Fruit_Item : MonoBehaviour
 {
+    private Animator anim;
+    [SerializeField] private SpriteRenderer sr;
+
+    public FruitType myFruitType;
+    [SerializeField] private Sprite[] fruitImage;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.GetComponent<Player>() != null)
@@ -11,5 +29,22 @@ public class Fruit_Item : MonoBehaviour
             collision.GetComponent<Player>().fruits++;
             Destroy(gameObject);
         }
+    }
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        for (int i=0; i<anim.layerCount; i++)
+        {
+            anim.SetLayerWeight(i, 0);
+        }
+
+        anim.SetLayerWeight(((int)myFruitType), 1);
+
+    }
+
+    private void OnValidate()
+    {
+        sr.sprite = fruitImage[((int)myFruitType)];
     }
 }
