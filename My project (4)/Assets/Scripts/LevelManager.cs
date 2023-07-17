535268467415b30a978c205bfc61aacc321a43e3
carlo0o0o <car.nota@stud.uniroma3.it>
2023-07-17 17:24:43 +0200

    level

--- My project (4)/Assets/Scripts/LevelManager.cs
+++ My project (4)/Assets/Scripts/LevelManager.cs
@@ -9,16 +9,20 @@ public class LevelManager : MonoBehaviour
 {
     [SerializeField] private GameObject levelButton;
     [SerializeField] private Transform levelButtonParent;
+    [SerializeField] private bool[] levelOpen;
 
     private void Start()
     {
         for(int i = 1; i < SceneManager.sceneCountInBuildSettings; i++)
         {
+            if (!levelOpen[i])
+                return;
+
             string sceneName = "Level " + i;
             Debug.Log(sceneName);
 
             GameObject newButton = Instantiate(levelButton, levelButtonParent);
-            newButton.AddComponent<Button>().onClick.AddListener(() => LoadLevel(sceneName));
+            newButton.GetComponent<Button>().onClick.AddListener(() => LoadLevel(sceneName));
             newButton.GetComponentInChildren<TextMeshProUGUI>().text = sceneName;
         }
     }
