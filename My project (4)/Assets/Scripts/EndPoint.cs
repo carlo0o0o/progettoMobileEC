0d6f09ebbe4d104d373f14635345265943808de7
carlo0o0o <car.nota@stud.uniroma3.it>
2023-07-19 02:30:53 +0200

    Pausa+risultatiPartita

--- My project (4)/Assets/Scripts/EndPoint.cs
+++ My project (4)/Assets/Scripts/EndPoint.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class EndPoint : MonoBehaviour
 {
+    private Timer_UI inGame_UI;
+    private void Start()
+    {
+        inGame_UI = GameObject.Find("Canvas").GetComponent<Timer_UI>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<Player>() != null)
         {
             GetComponent<Animator>().SetTrigger("activate");
-            Debug.Log("winner");
+
+            Destroy(collision.gameObject);
+
+            inGame_UI.OnLevelFinished();
+
             GameManager.instance.SaveBestTime();
             GameManager.instance.SaveCollectedFruits();
+
         }
     }
 }
--- My project (4)/Assets/Timer_UI.cs
+++ My project (4)/Assets/Timer_UI.cs
@@ -2,16 +2,86 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
-public class Timer_UI : MonoBehaviour
+public class Timer_UI : MonoBehaviour  //InGame_UI
 {
+
+    private bool gamePaused;
+
+    [Header("Menu gameObjects")]
+    [SerializeField] private GameObject inGameUI;
+    [SerializeField] private GameObject pauseUI;
+    [SerializeField] private GameObject endLevelUI;
+
+    [Header("Text Components")]
     [SerializeField] private TextMeshProUGUI timerText;
     [SerializeField] private TextMeshProUGUI currentFruitsAmout;
 
+    [SerializeField] private TextMeshProUGUI endTimerText;
+    [SerializeField] private TextMeshProUGUI endBestTimeText;
+    [SerializeField] private TextMeshProUGUI endFruitsText;
+
+
+    private void Start()
+    {
+        GameManager.instance.levelNumber = SceneManager.GetActiveScene().buildIndex;
+        Time.timeScale = 1;
+        SwitchUI(inGameUI);
+    }
 
     void Update()
     {
+        UpdateInGameInfo();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            CheckIsNotPaused();
+
+    }
+
+    private bool CheckIsNotPaused()
+    {
+        if (!gamePaused)
+        {
+            gamePaused = true;
+            Time.timeScale = 0;
+            SwitchUI(pauseUI);
+            return true;    
+        }
+        else
+        {
+            gamePaused = false;
+            Time.timeScale = 1;
+            SwitchUI(inGameUI);
+            return false;
+        }
+    }
+
+    public void OnLevelFinished()
+    {
+        endFruitsText.text = "Fruits: " + PlayerManager.instance.fruits;
+        endTimerText.text = "Your Time: " + GameManager.instance.timer.ToString("00") + " s";
+        endBestTimeText.text = "Best Time: " + PlayerPrefs.GetFloat("Level" + GameManager.instance.levelNumber + "BestTime", 999).ToString("00") + " s";
+        SwitchUI(endLevelUI);
+    }
+
+    private void UpdateInGameInfo(){
         timerText.text = "Timer: " + GameManager.instance.timer.ToString("00") + " s";
         currentFruitsAmout.text = PlayerManager.instance.fruits.ToString();
     }
+    public void SwitchUI(GameObject uiMenu)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        uiMenu.SetActive(true);
+    }
+
+    public void LoadMainMenu() => SceneManager.LoadScene("MainMenu");
+    public void ReloadCurrentLevel() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    public void LoadNextLevel() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
 }
